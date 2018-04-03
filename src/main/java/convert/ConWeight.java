22676152ccf05993266c05af396c3bec0dc26ec7
neyrobiolog <vpraskovya@bk.ru>
2018-04-04 00:49:55 +0600

    Добавление метода получения значения коэффициента конвертирования

--- src/main/java/convert/ConWeight.java
+++ src/main/java/convert/ConWeight.java
@@ -6,5 +6,20 @@ package convert;
  * @author neyrobiolog
  */
 public class ConWeight {
+        
+    //Конструктор класса
+    public ConWeight() {};
     
+    //Переменные для обращения к коэффициентам единиц измерения для конвертирования
+    //Система СИ
+    public static final int GRAMM = 0;      //Грамм
+    
+    /**
+     * Получить коэффициент конвертирования единицы измерения из массива по его индексу
+     * @param index наименование единицы измерения, тип int
+     * @return коэффициент конвертирования
+     */
+    public static double getValues(int index) {
+        return 0.001; 
+    }
 }
