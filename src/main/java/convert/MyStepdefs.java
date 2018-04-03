13bbfe9692be4a08a34ac45a055f3a2604b8de0c
neyrobiolog <vpraskovya@bk.ru>
2018-04-04 00:29:07 +0600

    Добавление файла steps definitions

---
+++ src/main/java/convert/MyStepdefs.java
@@ -0,0 +1,40 @@
+package convert;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+/**
+ *
+ * @author neyrobiolog
+ */
+public class MyStepdefs {
+    
+    private ConWeight weight;
+    private int GRAMM = weight.GRAMM;
+    private int operand1;
+    private double result;
+
+    @Given("^I have my software weight converter$")
+    public void iHaveMySoftwareWeightConverter() throws Throwable {
+        this.weight = new ConWeight();
+    }
+
+    @When("^I have entered GRAMM as first operand$")
+    public void iHaveEnteredAsFirstOperand() throws Throwable {
+        this.operand1 = GRAMM;
+    }
+
+    @And("^I press 'Get coefficient'$")
+    public void iPressGetCoefficient() throws Throwable {
+        this.result = weight.GetValue(operand1);
+    }
+
+    @Then("^The result should be (\\d+)$")
+    public void theResultShouldBe(int expected) throws Throwable {
+        Assert.assertEquals(expected, this.result, 1e-5);
+    }
+}
