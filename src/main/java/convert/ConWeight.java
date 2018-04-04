9d694bf7c012fbbff6e564041d2206702b158f95
neyrobiolog <vpraskovya@bk.ru>
2018-04-04 14:40:17 +0600

    Заглушка в методе getCoefficient для получения коэфф. квитала

--- src/main/java/convert/ConWeight.java
+++ src/main/java/convert/ConWeight.java
@@ -14,12 +14,17 @@ public class ConWeight {
     //Система СИ
     public static final int GRAMM = 0;      //Грамм
     
+    //Американская система
+    public static final int KVITAL = 3;     //Квитал
+    
     /**
      * Получить коэффициент конвертирования единицы измерения из массива по его индексу
      * @param index наименование единицы измерения, тип int
      * @return коэффициент конвертирования
      */
     public static double getCoefficient(int index) {
+        if (index == KVITAL) 
+                return 43.36;
         return 0.001; 
     }
 }
