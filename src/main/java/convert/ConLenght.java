866542620b3cf48b9cc16fc71d33079b138f1ebd
neyrobiolog <vpraskovya@bk.ru>
2018-04-04 21:52:44 +0600

    Создание класса Конвертер Длины

---
+++ src/main/java/convert/ConLenght.java
@@ -0,0 +1,12 @@
+package convert;
+
+/**
+ * Класс конвертирования величин длины.
+ * @author neyrobiolog
+ */
+public class ConLenght {
+    
+    //Конструктор класса
+    public ConLenght() {};
+      
+}
