8233b167c859c9297526652b5ddaaab8fdff73a8
neyrobiolog <vpraskovya@bk.ru>
2018-04-04 22:04:33 +0600

    Добавление метода getCoefficient для получения коэфф-та конвер-я сантиметров

--- src/main/java/convert/ConLenght.java
+++ src/main/java/convert/ConLenght.java
@@ -8,5 +8,18 @@ public class ConLenght {
     
     //Конструктор класса
     public ConLenght() {};
-      
+        
+    //Переменные для обращения к коэффициентам единиц измерения для конвертирования
+    //Система СИ
+    public static final int CANTIMETER = 0; //Сантиметр
+    
+    /**
+     * Получить коэффициент конвертирования единицы измерения по его индексу
+     * @param index наименование единицы измерения, тип int
+     * @return коэффициент конвертирования, тип double
+     */
+    public static double getCoefficient(int index) {
+       return 0.01; 
+    }
+    
 }
